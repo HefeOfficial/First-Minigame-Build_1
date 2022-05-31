abafd6003057a1d17bad6ba45dc5e433a3ebec17
HEFE <75194685+HefeOfficial@users.noreply.github.com>
2022-05-31 11:37:22 -0700

    Initialize character-select

---
+++ app/src/web3/actions/queries/index.tsx
@@ -0,0 +1,20 @@
+import { AavegotchiContractObject } from "types";
+
+export interface AavegotchisOfOwner {
+  aavegotchis: Array<AavegotchiContractObject>
+}
+
+export const getAllAavegotchisOfOwner = (owner: string) => {
+  const query = `
+    {
+      aavegotchis(first: 500, orderBy: withSetsRarityScore, orderDirection: desc,  where: { owner:"${owner.toLowerCase()}"}) {
+        id
+        name
+        withSetsNumericTraits
+        equippedWearables
+        withSetsRarityScore
+      }
+    }
+  `
+  return query;
+}
